67cf7bb8a243ecbd7a5e237ba69768dee355f4b6
Junzz <lijunzz@users.noreply.github.com>
2018-03-22 23:14:49 +0800

    初始版本

---
+++ app/src/main/java/net/junzz/app/recorder/RecorderActivity.java
@@ -0,0 +1,10 @@
+package net.junzz.app.recorder;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2018/3/22.
+ */
+
+public class RecorderActivity extends AppCompatActivity {
+}
